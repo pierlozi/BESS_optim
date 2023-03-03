fb66df4024944343155bf3746b9102e7092b63ed
Pierfrancesco ABB <pierfrancesco.losi@gmail.com>
2023-03-03 17:42:03 +0100

    GA + dispatcher SUCCESS!!

--- model_GA_dispatch_try1.py
+++ model_GA_dispatch_try1.py
@@ -1,15 +1,19 @@
 # %% 
 import time
+
 from pymoo.optimize import minimize
 from pymoo.core.problem import Problem
 from pymoo.algorithms.moo.nsga2 import NSGA2
-from pyomo.environ import *
+
+import pyomo.environ as pyo
 from pyomo.opt import SolverFactory
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#%%
+
+
 class microgrid():
     def __init__(self, Er_BES, Pr_BES, P_load, P_ren):
         self.Er_BES = Er_BES
@@ -17,7 +21,10 @@ class microgrid():
         self.P_load = P_load
         self.P_ren = P_ren
 
-def dispatcher_GA(design):
+def try_funct(x):
+    return [-x[0] - x[1], x[0] + x[1]]
+
+def dispatcher_GA(design): #outputs the LCOS
         
     size_font = 10
     mine_life = 13
@@ -73,98 +80,98 @@ def dispatcher_GA(design):
     I use it so that the model compiles different objective functions and constraints for the two types.'''
 
 
-    m = ConcreteModel()
+    m = pyo.ConcreteModel()
 
     #m.iIDX is the set which keeps the time in the simulation
-    m.iIDX = Set(initialize = time_range_optim)
+    m.iIDX = pyo.Set(initialize = time_range_optim)
 
     '''importing data in the pyomo framewrok''' 
-    m.P_load = Param(m.iIDX,initialize=P_load_dict)
-    m.P_prod = Param(m.iIDX, initialize=P_prod_dict)
-    m.price = Param(m.iIDX, initialize = price_dict)
+    m.P_load = pyo.Param(m.iIDX,initialize=P_load_dict)
+    m.P_prod = pyo.Param(m.iIDX, initialize=P_prod_dict)
+    #m.price = pyo.Param(m.iIDX, initialize = price_dict)
 
     '''initializing parameters for the simulation'''
 
     #the charging and dischargin efficiencies are calculeted using a roundrtip efficiency value
     #of 0.95 (Reference: Optimal sizing of battery energy storage in a microgrid considering capacity degradation and replacement year)
-    m.eff_ch = Param(initialize=sqrt(0.95)) 
-    m.eff_dch = Param(initialize=sqrt(0.95))
+    m.eff_ch = pyo.Param(initialize=sqrt(0.95)) 
+    m.eff_dch = pyo.Param(initialize=sqrt(0.95))
 
     # data from 'Optimal sizing of battery energy storage in a microgrid considering capacity degradation and replacement year'
 
-    # m.floatlife = Param(initialize=10) #years
-    # m.C_P = Param(initialize=320) #$/kW
-    # m.C_E = Param(initialize=360) #$/kWh
-    # m.C_inst = Param(initialize=15) #$/kWh
-    # m.C_POM = Param(initialize=5) #$/kW operation cost related to power
-    # m.C_EOM = Param(initialize=0) #$/Mwh operation cost related to energy
-    # m.sigma = Param(initialize=0.002/24) #original daily self discharge is 0,2% -> we need an hourly self discharge
-    # m.IR = Param(initialize = 5/100)
+    # m.floatlife = pyo.Param(initialize=10) #years
+    # m.C_P = pyo.Param(initialize=320) #$/kW
+    # m.C_E = pyo.Param(initialize=360) #$/kWh
+    # m.C_inst = pyo.Param(initialize=15) #$/kWh
+    # m.C_POM = pyo.Param(initialize=5) #$/kW operation cost related to power
+    # m.C_EOM = pyo.Param(initialize=0) #$/Mwh operation cost related to energy
+    # m.sigma = pyo.Param(initialize=0.002/24) #original daily self discharge is 0,2% -> we need an hourly self discharge
+    # m.IR = pyo.Param(initialize = 5/100)
 
     #data from 'Projecting the Future Levelized Cost of Electricity Storage Technologies'
-    m.floatlife = Param(initialize=10) #years
-    m.C_P = Param(initialize=678) #$/kW
-    m.C_E = Param(initialize=802) #$/kWh
-    m.C_inst = Param(initialize=0) #$/kWh (the reference doesnt take into account installation)
-    m.C_POM = Param(initialize=10) #$/kW operation cost related to power
-    m.C_EOM = Param(initialize=3) #$/Mwh operation cost related to energy
-    m.sigma = Param(initialize=0) #original daily self discharge is 0,2% -> we need an hourly self discharge
-    m.IR = Param(initialize = 8/100)
+    m.floatlife = pyo.Param(initialize=10) #years
+    m.C_P = pyo.Param(initialize=678) #$/kW
+    m.C_E = pyo.Param(initialize=802) #$/kWh
+    m.C_inst = pyo.Param(initialize=0) #$/kWh (the reference doesnt take into account installation)
+    m.C_POM = pyo.Param(initialize=10) #$/kW operation cost related to power
+    m.C_EOM = pyo.Param(initialize=3) #$/Mwh operation cost related to energy
+    m.sigma = pyo.Param(initialize=0) #original daily self discharge is 0,2% -> we need an hourly self discharge
+    m.IR = pyo.Param(initialize = 8/100)
 
     # '''Adding the table of DoD - cycle life to implement battery degradation'''
-    m.DoD = Param(initialize = 75)
-    m.cyclelife = Param(initialize = 2700)
+    m.DoD = pyo.Param(initialize = 75)
+    m.cyclelife = pyo.Param(initialize = 2700)
 
-    m.gamma_min = Param(initialize=0)
-    m.gamma_MAX = Param(initialize=100000) #maximum 10 (Stefan) -> for now I leave it very high to see how the system behaves
+    m.gamma_min = pyo.Param(initialize=0)
+    m.gamma_MAX = pyo.Param(initialize=100000) #maximum 10 (Stefan) -> for now I leave it very high to see how the system behaves
 
-    m.P_BES_MAX = Param(initialize=P_BES_MAX)
+    m.P_BES_MAX = pyo.Param(initialize=P_BES_MAX)
 
-    m.price_f = Param(initialize=1.66) #euro/L
+    m.price_f = pyo.Param(initialize=1.66) #euro/L
 
     #empirical parameters for diesel fuel consumption from 
     # "Multi objective particle swarm optimization of hybrid micro-grid system: A case study in Sweden"
-    m.alpha = Param(initialize=0.24) #L/kW
-    m.beta = Param(initialize=0.084) #L/kW
+    m.alpha = pyo.Param(initialize=0.24) #L/kW
+    m.beta = pyo.Param(initialize=0.084) #L/kW
 
     #minimum up and down time for diesel from
     # "Optimal sizing of battery energy storage systems in off-grid micro grids using convex optimization"
-    m.UT = Param(initialize = 5) #h
-    m.DT = Param(initialize = 1) #h
+    m.UT = pyo.Param(initialize = 5) #h
+    m.DT = pyo.Param(initialize = 1) #h
 
     #max and min power rating of the diesel generator, I choose it arbitrarily
-    m.Pr_dg_MAX = Param(initialize = max(P_load_data['Load [MW]']))
-    m.Pr_dg_MIN = Param(initialize = 0.1*sum(P_load_data['Load [MW]'])/len(P_load_data['Load [MW]']))
+    m.Pr_dg_MAX = pyo.Param(initialize = max(P_load_data['Load [MW]']))
+    m.Pr_dg_MIN = pyo.Param(initialize = 0.1*sum(P_load_data['Load [MW]'])/len(P_load_data['Load [MW]']))
 
 
 
     #with the BESS    
-    m.P_ch = Var(m.iIDX, domain=NonNegativeReals)
-    m.P_dch = Var(m.iIDX, domain = NonNegativeReals)
-    m.P_RES = Var(m.iIDX, domain = NonNegativeReals)
-    m.P_curt = Var(m.iIDX, domain = NonNegativeReals)
+    m.P_ch = pyo.Var(m.iIDX, domain=NonNegativeReals)
+    m.P_dch = pyo.Var(m.iIDX, domain = NonNegativeReals)
+    m.P_RES = pyo.Var(m.iIDX, domain = NonNegativeReals)
+    m.P_curt = pyo.Var(m.iIDX, domain = NonNegativeReals)
     
-    m.Pr_BES = Param(initialize = design.Pr_BES)
-    m.Er_BES = Param(initialize = design.Er_BES)
+    m.Pr_BES = pyo.Param(initialize = design.Pr_BES)
+    m.Er_BES = pyo.Param(initialize = design.Er_BES)
     
-    m.SOC = Var(m.iIDX, domain=NonNegativeReals)
-    m.SOC_ini = Var(domain=NonNegativeReals)
+    m.SOC = pyo.Var(m.iIDX, domain=NonNegativeReals)
+    m.SOC_ini = pyo.Var(domain=NonNegativeReals)
 
     # '''this is the bi-linear variable used to implement the DoD-cyclelife constraint'''
-    # m.LEr = Var(m.bIDX, domain = NonNegativeReals)
-    # m.chi = Var(m.bIDX, domain = Binary)
+    # m.LEr = pyo.Var(m.bIDX, domain = NonNegativeReals)
+    # m.chi = pyo.Var(m.bIDX, domain = Binary)
 
-    m.bin_dch = Var(m.iIDX, domain=Binary)
+    m.bin_dch = pyo.Var(m.iIDX, domain=Binary)
 
 
-    m.P_dg = Var(m.iIDX, domain = NonNegativeReals) #hourly power of diesel
-    m.Pr_dg = Var(domain=NonNegativeReals) #power rating of diesel
+    m.P_dg = pyo.Var(m.iIDX, domain = NonNegativeReals) #hourly power of diesel
+    m.Pr_dg = pyo.Var(domain=NonNegativeReals) #power rating of diesel
 
     # # these are the binary variables to be used for the min up/down times of the diesel generator
 
-    # m.v_dg = Var(m.iIDX, domain = Binary) #1 when dg turned on at timestep
-    # m.w_dg = Var(m.iIDX, domain = Binary) #1 when dg turned off at timestep
-    # m.u_dg = Var(m.iIDX, domain=Binary) # commitment of unit (1 if unit is on)
+    # m.v_dg = pyo.Var(m.iIDX, domain = Binary) #1 when dg turned on at timestep
+    # m.w_dg = pyo.Var(m.iIDX, domain = Binary) #1 when dg turned off at timestep
+    # m.u_dg = pyo.Var(m.iIDX, domain=Binary) # commitment of unit (1 if unit is on)
 
 
 
@@ -172,17 +179,17 @@ def dispatcher_GA(design):
     def obj_funct(m):
         return m.price_f*sum((m.alpha*m.Pr_dg + m.beta*m.P_dg[i])*1e3 for i in m.iIDX)
 
-    m.obj = Objective(rule = obj_funct, sense=minimize)
+    m.obj = pyo.Objective(rule = obj_funct, sense = pyo.minimize)
 
     def f_equi_RES(m,i):
         return m.P_prod[i] == m.P_RES[i] + m.P_ch[i] + m.P_curt[i]
 
-    m.cstr_eq_RES = Constraint(m.iIDX, rule = f_equi_RES)
+    m.cstr_eq_RES = pyo.Constraint(m.iIDX, rule = f_equi_RES)
 
     def f_equi_load(m,i):
         return m.P_dch[i] + m.P_RES[i] + m.P_dg[i] == m.P_load[i]
 
-    m.cstr_eq_load = Constraint(m.iIDX, rule = f_equi_load)
+    m.cstr_eq_load = pyo.Constraint(m.iIDX, rule = f_equi_load)
     
 
     # 'BATTERY CONSTRAINTS'#---------------------------------------------------------------------------------------------------------------
@@ -190,17 +197,17 @@ def dispatcher_GA(design):
     def f_SOC_lim_up(m,i):
         return m.SOC[i]<= m.Er_BES
 
-    m.cstr_SOC_lim_up = Constraint(m.iIDX, rule=f_SOC_lim_up)
+    m.cstr_SOC_lim_up = pyo.Constraint(m.iIDX, rule=f_SOC_lim_up)
 
     def f_SOC_lim_low(m,i):
         return m.SOC[i]>= m.Er_BES * ( 1 - m.DoD/100)
 
-    m.cstr_SOC_lim_low = Constraint(m.iIDX, rule=f_SOC_lim_low)
+    m.cstr_SOC_lim_low = pyo.Constraint(m.iIDX, rule=f_SOC_lim_low)
 
     def f_SOC_ini_lim(m):
         return m.SOC_ini <= m.Er_BES
 
-    m.cstr_SOC_ini_lim = Constraint(rule=f_SOC_ini_lim)
+    m.cstr_SOC_ini_lim = pyo.Constraint(rule=f_SOC_ini_lim)
 
     def f_SOC(m,i):
         if i == 0:
@@ -208,29 +215,29 @@ def dispatcher_GA(design):
         else:
             return m.SOC[i] == m.SOC[i-1]*(1-m.sigma) + m.P_ch[i]*m.eff_ch - m.P_dch[i]/m.eff_dch 
 
-    m.cstr_SOC = Constraint(m.iIDX, rule = f_SOC)
+    m.cstr_SOC = pyo.Constraint(m.iIDX, rule = f_SOC)
 
-    m.cstr_SOC_final = Constraint(expr = m.SOC[len(m.iIDX)-1]==m.SOC_ini )
+    m.cstr_SOC_final = pyo.Constraint(expr = m.SOC[len(m.iIDX)-1]==m.SOC_ini )
 
     def f_dch_lim(m,i):
         return m.P_dch[i] <= m.Pr_BES
 
-    m.cstr_dch_lim = Constraint(m.iIDX, rule = f_dch_lim)
+    m.cstr_dch_lim = pyo.Constraint(m.iIDX, rule = f_dch_lim)
 
     def f_dch_bin(m,i):
         return m.P_dch[i] <= m.bin_dch[i]*m.P_BES_MAX
 
-    m.cstr_dch_bin = Constraint(m.iIDX, rule=f_dch_bin)
+    m.cstr_dch_bin = pyo.Constraint(m.iIDX, rule=f_dch_bin)
 
     def f_ch_lim(m,i):
         return m.P_ch[i] <= m.Pr_BES
 
-    m.cstr_ch_lim = Constraint(m.iIDX, rule = f_ch_lim)
+    m.cstr_ch_lim = pyo.Constraint(m.iIDX, rule = f_ch_lim)
 
     def f_ch_bin(m,i):
         return m.P_ch[i] <= (1-m.bin_dch[i])*m.P_BES_MAX
 
-    m.cstr_ch_bin = Constraint(m.iIDX, rule=f_ch_bin)
+    m.cstr_ch_bin = pyo.Constraint(m.iIDX, rule=f_ch_bin)
 
 
     '----------------------------------------------------------------------------------------------------------------------------'
@@ -238,20 +245,20 @@ def dispatcher_GA(design):
     def f_dg_lim(m,i):
         return m.P_dg[i] <= m.Pr_dg
 
-    m.cstr_dg_lim = Constraint(m.iIDX, rule=f_dg_lim)
+    m.cstr_dg_lim = pyo.Constraint(m.iIDX, rule=f_dg_lim)
 
     # def f_dg_commit_sup(m, i):
     #     return m.P_dg[i] <= m.u_dg[i]*m.Pr_dg_MAX
 
-    # m.cstr_dg_commit_sup = Constraint(m.iIDX, rule=f_dg_commit_sup)
+    # m.cstr_dg_commit_sup = pyo.Constraint(m.iIDX, rule=f_dg_commit_sup)
 
     # def f_dg_commit_inf(m, i):
     #     return m.P_dg[i] >= m.u_dg[i]*m.Pr_dg_MIN
 
-    # m.cstr_dg_commit_inf = Constraint(m.iIDX, rule=f_dg_commit_inf)
+    # m.cstr_dg_commit_inf = pyo.Constraint(m.iIDX, rule=f_dg_commit_inf)
 
 
-    # m.cstr_dg_uptime = ConstraintList()
+    # m.cstr_dg_uptime = pyo.ConstraintList()
 
     # # def f_dg_uptime(m, i):
     # #     return sum(m.u[j] for j in range(i, i + m.UT) ) >= m.UT*m.v_dg[i]
@@ -260,7 +267,7 @@ def dispatcher_GA(design):
     #     if i <= len(m.iIDX) - m.UT - 1:
     #         m.cstr_dg_uptime.add(sum(m.u_dg[j] for j in range(i, i + m.UT) ) >= m.UT*m.v_dg[i])
 
-    # m.cstr_dg_dwntime = ConstraintList()
+    # m.cstr_dg_dwntime = pyo.ConstraintList()
 
     # # def f_dg_dwntime(m, i):
     # #     return sum((1 - m.u_dg[j]) for j in range(i, i + m.UT) ) >= m.DT*m.w_dg[i]
@@ -269,7 +276,7 @@ def dispatcher_GA(design):
     #     if i <= len(m.iIDX) - m.DT - 1:
     #         m.cstr_dg_dwntime.add(sum((1 - m.u_dg[j]) for j in range(i, i + m.DT) ) >= m.DT*m.w_dg[i]
     # )   
-    # m.cstr_up_dwn_commit = ConstraintList()
+    # m.cstr_up_dwn_commit = pyo.ConstraintList()
 
     # # def f_up_dwn_commit(m, i):
     # #     return m.v_dg[i] - m.w_dg[i] == m.u_dg[i] - m.u_dg[i-1]
@@ -281,12 +288,12 @@ def dispatcher_GA(design):
     # def f_up_dwn_excl(m, i):
     #     return m.v_dg[i] + m.w_dg[i] <= 1
 
-    # m.cstr_up_dwn_excl = Constraint(m.iIDX, rule = f_up_dwn_excl)
+    # m.cstr_up_dwn_excl = pyo.Constraint(m.iIDX, rule = f_up_dwn_excl)
 
     # Initializing results lists
 
     start = time.time()
-    opt = SolverFactory("gurobi")
+    opt = pyo.SolverFactory("gurobi")
     opt.solve(m)
     code_time.append( time.time() - start)
 
@@ -358,6 +365,9 @@ def dispatcher_GA(design):
 
     return LCOS 
 
+
+#%%
+
 ''' Reading the data from csv files'''
 P_load_data = pd.read_excel('load_data.xlsx', sheet_name='Yearly Load', header=0) #MW
 P_ren_read = pd.read_csv('RESData_option-2.csv', header=0, nrows = 8760) #W
@@ -367,30 +377,23 @@ LCOS = dispatcher_GA(design)
 print(LCOS)
 
 #%%
-def try_funct(x):
-    return [- x[0] - x[1] , x[0] + x[1]]
-
-xtry = [np.linspace(-10,10,21), np.linspace(-10,10,21)]
-y1 = try_funct(xtry)[0]
-y2 = try_funct(xtry)[1]
-
-plt.plot(y1,y2)
-plt.show()
 
 class ProblemWrapper(Problem):
 
-    def _evaluate(self, xs, out, *args, **kwargs):
+    def _evaluate(self, designs, out, *args, **kwargs):
         res = []
-        for x in xs:
-            res.append(try_funct(x))
+
+        for x in designs:
+            design = microgrid(Pr_BES=x[1],Er_BES=x[0], P_load=P_load_data, P_ren=P_ren_read)
+            res.append(dispatcher_GA(design))
         
         out['F'] = np.array(res)
 
-prob = ProblemWrapper(n_var=2, n_obj=2, xl=[-200.,-25.], xu = [200.,25.])
+prob = ProblemWrapper(n_var=2, n_obj=1, xl=[190.,15.], xu = [210.,20.])
 
-algo = NSGA2(pop_size=200)
+algo = NSGA2(pop_size=10)
 
-stop_criterium = ('n_gen',100)
+stop_criterium = ('n_gen',2)
 
 results = minimize(
     problem=prob,
@@ -401,6 +404,8 @@ results = minimize(
 y = results.F.T
 x = results.X
 
-plt.scatter(y[0], y[1])
-plt.show()
+print(x)
+print(y)
+# plt.scatter(y[0], y[1])
+# plt.show()
 # %%
