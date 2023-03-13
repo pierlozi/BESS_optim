8e24b2d8a9be631b8d15232eac3049d7076e61a0
Pierfrancesco ABB <pierfrancesco.losi@gmail.com>
2023-03-13 13:11:28 +0100

    pareto optimal fuel vs LCOS

--- model_GA_dispatch_try1.py
+++ model_GA_dispatch_try1.py
@@ -12,6 +12,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from math import sqrt
 
 
 class microgrid():
@@ -24,7 +25,7 @@ class microgrid():
 def try_funct(x):
     return [-x[0] - x[1], x[0] + x[1]]
 
-def dispatcher_GA(design): #outputs the LCOS
+def dispatcher_GA(design): #outputs the LCOS and DIESEL_CONS
         
     size_font = 10
     mine_life = 13
@@ -69,7 +70,6 @@ def dispatcher_GA(design): #outputs the LCOS
         
     P_prod_dict = dict()
     for i in time_range_optim:
-        P_prod_dict[i] = P_prod_data[i] #MW
         P_prod_dict[i] = P_prod_data[i] #MW
 
 
@@ -146,26 +146,26 @@ def dispatcher_GA(design): #outputs the LCOS
 
 
     #with the BESS    
-    m.P_ch = pyo.Var(m.iIDX, domain=NonNegativeReals)
-    m.P_dch = pyo.Var(m.iIDX, domain = NonNegativeReals)
-    m.P_RES = pyo.Var(m.iIDX, domain = NonNegativeReals)
-    m.P_curt = pyo.Var(m.iIDX, domain = NonNegativeReals)
+    m.P_ch = pyo.Var(m.iIDX, domain=pyo.NonNegativeReals)
+    m.P_dch = pyo.Var(m.iIDX, domain = pyo.NonNegativeReals)
+    m.P_RES = pyo.Var(m.iIDX, domain = pyo.NonNegativeReals)
+    m.P_curt = pyo.Var(m.iIDX, domain = pyo.NonNegativeReals)
     
     m.Pr_BES = pyo.Param(initialize = design.Pr_BES)
     m.Er_BES = pyo.Param(initialize = design.Er_BES)
     
-    m.SOC = pyo.Var(m.iIDX, domain=NonNegativeReals)
-    m.SOC_ini = pyo.Var(domain=NonNegativeReals)
+    m.SOC = pyo.Var(m.iIDX, domain=pyo.NonNegativeReals)
+    m.SOC_ini = pyo.Var(domain=pyo.NonNegativeReals)
 
     # '''this is the bi-linear variable used to implement the DoD-cyclelife constraint'''
     # m.LEr = pyo.Var(m.bIDX, domain = NonNegativeReals)
     # m.chi = pyo.Var(m.bIDX, domain = Binary)
 
-    m.bin_dch = pyo.Var(m.iIDX, domain=Binary)
+    m.bin_dch = pyo.Var(m.iIDX, domain=pyo.Binary)
 
 
-    m.P_dg = pyo.Var(m.iIDX, domain = NonNegativeReals) #hourly power of diesel
-    m.Pr_dg = pyo.Var(domain=NonNegativeReals) #power rating of diesel
+    m.P_dg = pyo.Var(m.iIDX, domain = pyo.NonNegativeReals) #hourly power of diesel
+    m.Pr_dg = pyo.Var(domain=pyo.NonNegativeReals) #power rating of diesel
 
     # # these are the binary variables to be used for the min up/down times of the diesel generator
 
@@ -298,42 +298,42 @@ def dispatcher_GA(design): #outputs the LCOS
     code_time.append( time.time() - start)
 
 
-    P_prod = np.array([value(m.P_prod[i]) for i in m.iIDX])
-    P_load = np.array([value(m.P_load[i]) for i in m.iIDX])
+    P_prod = np.array([pyo.value(m.P_prod[i]) for i in m.iIDX])
+    P_load = np.array([pyo.value(m.P_load[i]) for i in m.iIDX])
         
-    SOC.append(np.array([value(m.SOC[i]) for i in m.iIDX])/value(m.Er_BES))
-    P_BES.append(np.array([(value(m.P_dch[i]) - value(m.P_ch[i]) ) for i in m.iIDX]))
-    P_dch.append(value(sum(m.P_dch[i] for i in m.iIDX)))
-    BES_capex.append(value((m.Pr_BES*m.C_P + m.Er_BES*(m.C_E+m.C_inst))*1e3)) #€
-    BES_opex.append(value(m.Pr_BES*m.C_POM*1e3 + m.Er_BES*m.C_EOM)) #€/year
-    Er_BES.append(value(m.Er_BES))
-    Pr_BES.append(value(m.Pr_BES))
+    SOC.append(np.array([pyo.value(m.SOC[i]) for i in m.iIDX])/pyo.value(m.Er_BES))
+    P_BES.append(np.array([(pyo.value(m.P_dch[i]) - pyo.value(m.P_ch[i]) ) for i in m.iIDX]))
+    P_dch.append(pyo.value(sum(m.P_dch[i] for i in m.iIDX)))
+    BES_capex.append(pyo.value((m.Pr_BES*m.C_P + m.Er_BES*(m.C_E+m.C_inst))*1e3)) #€
+    BES_opex.append(pyo.value(m.Pr_BES*m.C_POM*1e3 + m.Er_BES*m.C_EOM)) #€/year
+    Er_BES.append(pyo.value(m.Er_BES))
+    Pr_BES.append(pyo.value(m.Pr_BES))
 
     # chi = np.array([value(m.chi[b]) for b in m.bIDX])
     # LEr = np.array([value(m.LEr[b]) for b in m.bIDX])
         
-    P_curt.append(np.array([value(m.P_curt[i]) for i in m.iIDX]))
+    P_curt.append(np.array([pyo.value(m.P_curt[i]) for i in m.iIDX]))
 
-    P_dg.append(np.array([value(m.P_dg[i]) for i in m.iIDX]))  
-    dg_opex.append(value((m.price_f*sum((m.alpha*m.Pr_dg + m.beta*m.P_dg[i])*1e3 for i in m.iIDX)))) #€/year   
-    Pr_dg.append(value(m.Pr_dg))
-    # u_dg = np.array([value(m.u_dg[i]) for i in m.iIDX])
-    # w_dg = np.array([value(m.w_dg[i]) for i in m.iIDX])
-    # v_dg = np.array([value(m.v_dg[i]) for i in m.iIDX])
+    P_dg.append(np.array([pyo.value(m.P_dg[i]) for i in m.iIDX]))  
+    dg_opex.append(pyo.value((m.price_f*sum((m.alpha*m.Pr_dg + m.beta*m.P_dg[i])*1e3 for i in m.iIDX)))) #€/year   
+    Pr_dg.append(pyo.value(m.Pr_dg))
+    # u_dg = np.array([pyo.value(m.u_dg[i]) for i in m.iIDX])
+    # w_dg = np.array([pyo.value(m.w_dg[i]) for i in m.iIDX])
+    # v_dg = np.array([pyo.value(m.v_dg[i]) for i in m.iIDX])
 
     P_thr.append(abs(P_BES[-1]))
 
     cycles_y.append(sum(P_thr[-1])/Er_BES[-1])
 
-    BES_cyclelife.append(round(value(m.cyclelife)/cycles_y[-1]))
+    BES_cyclelife.append(round(pyo.value(m.cyclelife)/cycles_y[-1]))
 
     cost_cash_flow  = []
     for i in range(0, mine_life):
-        if mine_life > value(m.floatlife): #if the mine has a life longer than shelflife of battery
-            if BES_cyclelife[-1] >= value(m.floatlife): #if battery has to be changed at floatlife
+        if mine_life > pyo.value(m.floatlife): #if the mine has a life longer than shelflife of battery
+            if BES_cyclelife[-1] >= pyo.value(m.floatlife): #if battery has to be changed at floatlife
                 if i == 0:
                     cost_cash_flow.append(BES_capex[-1] + BES_opex[-1]) # + dg_opex[-1]) #€
-                elif i == value(m.floatlife) - 1:    
+                elif i == pyo.value(m.floatlife) - 1:    
                     cost_cash_flow.append(BES_capex[-1] + BES_opex[-1]) # + dg_opex[-1]) #€
                 else:
                     cost_cash_flow.append(BES_opex[-1]) # + dg_opex[-1]) #€
@@ -353,17 +353,19 @@ def dispatcher_GA(design): #outputs the LCOS
 
     cost_dsctd = [] #€
     for i in range(len(cost_cash_flow)):
-        cost_dsctd.append(cost_cash_flow[i]/(1+value(m.IR))**i)
+        cost_dsctd.append(cost_cash_flow[i]/(1+pyo.value(m.IR))**i)
 
     dsctd_cash_flows.append(cost_dsctd)
 
     P_dch_dsctd = [] #MWh
     for i in range(len(cost_cash_flow)):
-        P_dch_dsctd.append(P_dch[-1]/(1+value(m.IR))**i)
+        P_dch_dsctd.append(P_dch[-1]/(1+pyo.value(m.IR))**i)
 
     LCOS = sum(cost_dsctd)/sum(P_dch_dsctd) #€/MWh
 
-    return LCOS 
+    EM_COST = pyo.value(sum((m.alpha*m.Pr_dg + m.beta*m.P_dg[i])*1e3 for i in m.iIDX))
+
+    return [LCOS, EM_COST]
 
 
 #%%
@@ -373,7 +375,8 @@ P_load_data = pd.read_excel('load_data.xlsx', sheet_name='Yearly Load', header=0
 P_ren_read = pd.read_csv('RESData_option-2.csv', header=0, nrows = 8760) #W
 
 design = microgrid(Pr_BES=17.7,Er_BES=173, P_load=P_load_data, P_ren=P_ren_read)
-LCOS = dispatcher_GA(design)
+[LCOS, DG_CONS] = dispatcher_GA(design)
+
 print(LCOS)
 
 #%%
@@ -389,11 +392,11 @@ class ProblemWrapper(Problem):
         
         out['F'] = np.array(res)
 
-prob = ProblemWrapper(n_var=2, n_obj=1, xl=[190.,15.], xu = [210.,20.])
+prob = ProblemWrapper(n_var=2, n_obj=2, xl=[0.,0.], xu = [2000.,116.])
 
-algo = NSGA2(pop_size=10)
+algo = NSGA2(pop_size=60)
 
-stop_criterium = ('n_gen',2)
+stop_criterium = ('n_gen',20)
 
 results = minimize(
     problem=prob,
@@ -401,11 +404,48 @@ results = minimize(
     termination=stop_criterium
 )
 
-y = results.F.T
-x = results.X
 
-print(x)
-print(y)
-# plt.scatter(y[0], y[1])
+X = results.X
+F = results.F
+
+pareto_opt_gen = pd.DataFrame({'Capacity [MWh]': results.X[:,0],
+                         'Power [MW]': results.X[:,1] ,
+                         'LCOS [€/MWh]': results.F[:,0],
+                         'Fuel [L]': results.F[:,1]
+                        })
+
+pareto_opt_gen.to_excel('pareto_GA_60p_20g.xlsx')
+
+xl, xu = prob.bounds()
+plt.figure(figsize=(7, 5))
+plt.scatter(X[:, 0], X[:, 1], s=40, facecolors='none', edgecolors='r', label = "Pareto optimal solutions")
+plt.plot(X[:, 0], X[:, 0]/10, label= "10 hours storage")
+plt.xlim(xl[0], xu[0])
+plt.ylim(xl[1], xu[1])
+plt.title("Design Space")
+plt.xlabel("Energy rating [MWh]")
+plt.ylabel("Power rating [MW]")
+plt.legend(loc = "best")
+plt.show()
+
+approx_ideal = F.min(axis=0) # gives an array with the minimum for every column
+approx_nadir = F.max(axis=0)
+
+plt.figure(figsize=(7, 5))
+plt.scatter(F[:, 0], F[:, 1], s=30, facecolors='none', edgecolors='blue')
+plt.scatter(approx_ideal[0], approx_ideal[1], facecolors='none', edgecolors='red', marker="*", s=100, label="Ideal Point (Approx)")
+plt.scatter(approx_nadir[0], approx_nadir[1], facecolors='none', edgecolors='black', marker="p", s=100, label="Nadir Point (Approx)")
+
+plt.title("Objective Space")
+plt.xlabel("LCOS [€/MWh]")
+plt.ylabel("Fuel consumption [L]")
+plt.legend(loc = "best")
+plt.show()
+
+# nF = (F - approx_ideal) / (approx_nadir - approx_ideal)
+
+# plt.figure(figsize=(7, 5))
+# plt.scatter(nF[:, 0], nF[:, 1], s=30, facecolors='none', edgecolors='blue')
+# plt.title("Normalized Objective Space")
 # plt.show()
 # %%
