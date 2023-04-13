6d0d4fc9582574b8a1dec59fb793c5635f6d1cc0
Pierfrancesco ABB <pierfrancesco.losi@gmail.com>
2023-04-13 15:06:58 +0200

    minor updates

--- Tests/Test6/test_GA_parallel.py
+++ Tests/Test6/test_GA_parallel.py
@@ -2,48 +2,50 @@
 import os
 import sys
 sys.path.append(r"C:\Users\SEPILOS\OneDrive - ABB\Documents\Projects\Model")
-from Core import dispatcher_GA
+from Core import dispatcher_GA, dispatcher_SOC_pen
 from Core import microgrid_design
 from Core import day_of_year
 
 RES_data_file_path = r"C:\Users\SEPILOS\OneDrive - ABB\Documents\Projects\Model\InputData\RESData_option-2.csv"
 
 
+from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.optimize import minimize
 from pymoo.core.problem import Problem
-from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.core.problem import ElementwiseProblem
+
+from pymoo.operators.sampling.rnd import IntegerRandomSampling
 from pymoo.termination import get_termination
-from multiprocessing.pool import ThreadPool
+
 from pymoo.core.problem import StarmapParallelization
+
+from multiprocessing.pool import ThreadPool
 
 import numpy as np
 import pandas as pd
 
-''' Reading the data from csv files'''
-
-P_ren_read = pd.read_csv(RES_data_file_path, header=0, nrows = 8760) #W
-P_load_data = pd.DataFrame()
-P_load_data['Load [MW]'] = P_ren_read['Power'].mean()*np.ones(len(P_ren_read))/1e6
-
-pool = ThreadPool(os.cpu_count())
+#I am parallelizing the code in a thread pool of as many threads as CPU cores there are
+#pool = ThreadPool(os.cpu_count()) 
+pool = ThreadPool(8)
 
 class MyProblem(Problem):
 
     def __init__(self, design, **kwargs):
-        self.design = design  # store the design object as an attribute
-        super().__init__(n_var=3, n_obj=2, xl=[0.,0.,20.], xu = [2000.,200.,80.], **kwargs)
+        self.design = design  # store the microgrid design object as an attribute 
+        super().__init__(n_var=3, n_obj=2, xl=[0.,0.,20.], xu = [2000.,200.,80.], vtype=int, **kwargs)
 
     def _evaluate(self, X, out, *args, **kwargs):
-        print('X[:,0]:', X[:,0])
-        print('X[:,1]:', X[:,1])
-        print('X[:,2]:', X[:,2])
+        #just prints of X components to see what's inside
+        # print('X[:,0]:', X[:,0])
+        # print('X[:,1]:', X[:,1])
+        # print('X[:,2]:', X[:,2])
 
         # prepare the parameters for the pool
         params = [(self.design, False, X[k, 0], X[k, 1], X[k, 2]) for k in range(len(X))]
-        print('params:', params)
+        #print('params:', params)
 
         F = pool.starmap(dispatcher_GA.MyFun, params)
-        print('F:', F)
+        #print('F:', F)
         # store the function values and return them.
         out["F"] = np.array(F)
 
@@ -77,20 +79,33 @@ class MyProblem(Problem):
     #     # out is a dictionary of which "F" is a key
     #     out["F"] = np.array(F)
 
+''' Reading the data from csv files'''
+
+P_ren_read = pd.read_csv(RES_data_file_path, header=0, nrows = 8760) #W
+P_load_data = pd.DataFrame()
+P_load_data['Load [MW]'] = P_ren_read['Power'].mean()*np.ones(len(P_ren_read))/1e6
+
 problem = MyProblem(design = microgrid_design.MG(P_load=P_load_data, \
                                                  P_ren=P_ren_read, \
                                                 )
                     )
 
-#
+#%%
 
-algorithm = NSGA2(pop_size=5)
+algorithm = NSGA2(pop_size=3,
+                  sampling = IntegerRandomSampling(),
+                  eliminate_duplicates=True
+                  )
 
-res = minimize(problem, 
+#%%
+
+results = minimize(problem, 
                algorithm,
                termination=("n_gen", 1), 
-               seed=1)
-print('Time:', res.exec_time)
+               seed=1
+               )
+
+print('Execution Time:', results.exec_time)
 pool.close()
 
 # %%
